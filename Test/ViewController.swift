51f291fcafacdd0fa86e4d616ceec7250e61cfc0
yukiees <yukiii8989@gmail.com>
2020-05-17 15:00:02 +0900

    Test

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var listlist = [1,2,3,4,5]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var listNumber: UILabel!
+    @IBOutlet weak var listName: UILabel!
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
+    @IBOutlet weak var thirdButton: UIButton!
+    @IBOutlet weak var pre: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    @IBAction func firstButtonPushed() {
+        print(listlist[0])
+    }
+    
+    
+    
 }
 
