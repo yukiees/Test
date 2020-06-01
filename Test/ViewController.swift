9bf711c7ec92232d12e3d45732e2bc9d697b3630
yukiees <yukiii8989@gmail.com>
2020-06-02 01:22:49 +0900

    test complete

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -10,13 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var listlist = [1,2,3,4,5]
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
     @IBOutlet weak var listNumber: UILabel!
     @IBOutlet weak var listName: UILabel!
     @IBOutlet weak var firstButton: UIButton!
@@ -25,11 +18,53 @@ class ViewController: UIViewController {
     @IBOutlet weak var pre: UIButton!
     @IBOutlet weak var nextButton: UIButton!
     
+    var listlist: [String] = ["A","B","C","D","E"]
+    var number: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
     @IBAction func firstButtonPushed() {
-        print(listlist[0])
+        number = 0
+        listNumber.text = "第\(number+1)問"
+        listName.text = listlist[number]
     }
     
+    @IBAction func secondButtonPushed() {
+        number = 1
+        listNumber.text = "第\(number+1)問"
+        listName.text = listlist[number]
+    }
     
+    @IBAction func thirdButtonPushed() {
+        number = 2
+        listNumber.text = "第\(number+1)問"
+        listName.text = listlist[number]
+    }
+    
+    @IBAction func preButton(){
+        if number == 0{
+            number = 4
+        }else{
+            number -= 1
+        }
+        listNumber.text = "第\(number+1)問"
+        listName.text = listlist[number]
+    }
+    
+    @IBAction func tappednextButton(){
+        if number == 4{
+            number = 0
+        }else{
+            number += 1
+        }
+        listNumber.text = "第\(number+1)問"
+        listName.text = listlist[number]
+    }
     
 }
 
